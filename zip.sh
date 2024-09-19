37aa26c5603a046e4ca3611c74d32e8f6ef90a63
Athi07 <58383546+Athi07@users.noreply.github.com>
2024-09-19 17:26:16 +0530

    Update zip.sh

--- zip.sh
+++ zip.sh
@@ -1,5 +1,5 @@
 cp out/arch/arm64/boot/Image ./anykernel/
 
-cd anykernel && zip -r Bigshot-kernel-$(date +"%d-%m-%Y"-%H%M).zip * && mv Bigshot-kernel-$(date +"%d-%m-%Y-%H%M").zip ../out/
+cd anykernel && zip -r Phoenix-kernel-$(date +"%d-%m-%Y"-%H%M).zip * && mv Phoenix-kernel-$(date +"%d-%m-%Y-%H%M").zip ../out/
 
 cd ..
