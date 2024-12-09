0e086c737925b52f37987cf45874fb46c7d5ebef
Athi07 <veldanda.athirath@gmail.com>
2024-12-10 01:12:29 +0530

    treewide: Fix LLVM 18 warns

--- techpack/display/oplus/oplus_display_panel.c
+++ techpack/display/oplus/oplus_display_panel.c
@@ -487,4 +487,4 @@ void __exit oplus_display_panel_exit(void)
 module_init(oplus_display_panel_init);
 module_exit(oplus_display_panel_exit);
 MODULE_LICENSE("GPL v2");
-MODULE_AUTHOR("Lisheng");
+MODULE_AUTHOR("Lisheng <lisheng1@oplus.com>");
